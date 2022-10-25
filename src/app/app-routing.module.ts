14d11bacf23f12a39981e56c43173b160db71806
freizeit-team-dresden <103765182+freizeit-team-dresden@users.noreply.github.com>
2022-10-25 21:54:13 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { ImpressumComponent } from './impressum/impressum.component';
 import { RulesComponent } from './rules/rules.component';
 import { RegisterComponent } from './register/register.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { NewsComponent } from './news/news.component';
 import { EventsComponent } from './events/events.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'events', component: EventsComponent },
   { path: 'about-us', component: AboutUsComponent },
+  { path: 'news', component: NewsComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'rules', component: RulesComponent },
   { path: 'impressum', component: ImpressumComponent },
